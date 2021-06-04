e388bb53116b6eacdedff8691ff9cf28a84f5fc3
Masayuki Kawasaki <nu11p0er@gmail.com>
2021-06-04 15:05:33 +0900

    Changed to not use unified memory

--- cuda.cu
+++ cuda.cu
@@ -20,37 +20,53 @@ int main(int argc, char **argv) {
 
     printf("Vector size: %d\n\n", nElem);
 
-    // malloc host and unified memory
-    float *A, *B, *hostRef, *gpuRef;
-    CHECK(cudaMallocManaged((void **)&A, nBytes));
-    CHECK(cudaMallocManaged((void **)&B, nBytes));
-    CHECK(cudaMallocManaged((void **)&hostRef, nBytes));
-    CHECK(cudaMallocManaged((void **)&gpuRef, nBytes));
+    // malloc host memory
+    float *h_A, *h_B, *hostRef, *gpuRef;
+    h_A     = (float *)malloc(nBytes);
+    h_B     = (float *)malloc(nBytes);
+    hostRef = (float *)malloc(nBytes);
+    gpuRef  = (float *)malloc(nBytes);
 
-    initialData(A, nElem);
-    initialData(B, nElem);
+    initialData(h_A, nElem);
+    initialData(h_B, nElem);
 
-    sumArraysOnHost(A, B, hostRef, nElem);
+    sumArraysOnHost(h_A, h_B, hostRef, nElem);
+
+    // malloc device global memory
+    float *d_A, *d_B, *d_C;
+    CHECK(cudaMalloc((float **)&d_A, nBytes));
+    CHECK(cudaMalloc((float **)&d_B, nBytes));
+    CHECK(cudaMalloc((float **)&d_C, nBytes));
+
+    // transfer data from host to device
+    CHECK(cudaMemcpy(d_A, h_A, nBytes, cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(d_B, h_B, nBytes, cudaMemcpyHostToDevice));
 
     dim3 block(1024);
     dim3 grid((nElem + block.x - 1) / block.x);
 
     // warmup
-    sumArraysOnGPU<<<grid, block>>>(A, B, gpuRef, nElem);
+    sumArraysOnGPU<<<grid, block>>>(d_A, d_B, d_C, nElem);
     CHECK(cudaDeviceSynchronize());
 
     double dtime = - omp_get_wtime();
-    sumArraysOnGPU<<<grid, block>>>(A, B, gpuRef, nElem);
+    sumArraysOnGPU<<<grid, block>>>(d_A, d_B, d_C, nElem);
     CHECK(cudaDeviceSynchronize());
     dtime += omp_get_wtime();
     printf("\"sumArraysOnGPU\" with <<<grid %d, block %d>>>\n", grid.x, block.x);
     printf("Elapsed time: %.3f sec, %lf GFLOPS\n\n", dtime, COST * nElem / dtime / 1.0e+9);
+
+    CHECK(cudaMemcpy(gpuRef, d_C, nBytes, cudaMemcpyDeviceToHost));
     checkResult(hostRef, gpuRef, nElem);
 
-    CHECK(cudaFree(A));
-    CHECK(cudaFree(B));
-    CHECK(cudaFree(hostRef));
-    CHECK(cudaFree(gpuRef));
+    free(h_A);
+    free(h_B);
+    free(hostRef);
+    free(gpuRef);
+
+    CHECK(cudaFree(d_A));
+    CHECK(cudaFree(d_B));
+    CHECK(cudaFree(d_C));
 
     CHECK(cudaDeviceReset());
 
