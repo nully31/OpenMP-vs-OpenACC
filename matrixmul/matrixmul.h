bf6ed6d1afa8772bf878a8f95fbb5e31e7e2da2c
Masayuki Kawasaki <nu11p0er@gmail.com>
2022-01-18 14:00:17 +0900

    Add clang support

--- matrixmul/matrixmul.h
+++ matrixmul/matrixmul.h
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
 #ifndef _COMMON_H_
 #define _COMMON_H_
@@ -21,7 +22,7 @@ void checkResult(float *hostRef, float *gpuRef, const int N) {
     double epsilon = 1.0e-1;
     bool match = 1;
     for (int i = 0; i < N; i++) {
-        if (abs(hostRef[i] - gpuRef[i]) > epsilon) {
+        if (fabs(hostRef[i] - gpuRef[i]) > epsilon) {
             match = 0;
             printf("Arrays do not match!\n");
             printf("host %5.2f\tgpu %5.2f at current %d\n", hostRef[i], gpuRef[i], i);
