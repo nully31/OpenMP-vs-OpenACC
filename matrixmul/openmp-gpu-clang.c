5ce98eac8c53e30b2d7c314786945e98927dab5e
Masayuki Kawasaki <nu11p0er@gmail.com>
2022-01-29 01:21:33 +0900

    Support for clang with optimization flags

--- matrixmul/openmp-gpu-clang.c
+++ matrixmul/openmp-gpu-clang.c
@@ -1,27 +1,26 @@
 /*
- * clang 14.0 crashes on compile when any optimization options (except `-O0`) are involved for some reason.
- * Use `make openmp-gpu-clang-opt` for optimized binary.
+ * On clang 14.0, if the function `mulMatrixOnACC()` is absent from the code even if it's not used anywhere, the comiler crashes (like what??? how???).
+ * Otherwise, you have to disable optimization flags completely.
+ * Really weird bug of clang 14.0 with GPU offloading.
  */
 
 #include <omp.h>
 #include <cblas.h>
 #include "matrixmul.h"
 
-void mulMatrixOnACC(float *A, float *B, float *D, int nElem)
-{
+void mulMatrixOnACC(float *A, float *B, float *C, const int N) {
     #pragma omp target teams distribute \
             parallel for collapse(2)
-    for (int i = 0; i < nElem; i++) {
-        for (int j = 0; j < nElem; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             float temp = 0.0;
-            for (int k = 0; k < nElem; k++) {
-                temp += A[i * nElem + k] * B[k * nElem + j];
+            for (int k = 0; k < N; k++) {
+                temp += A[i * N + k] * B[k * N + j];
             }
-            D[i * nElem + j] = temp;
+            C[i * N + j] = temp;
         }
     }
 }
-
 
 int main(int argc, char const *argv[])
 {
@@ -56,7 +55,17 @@ int main(int argc, char const *argv[])
 
     printf("\033[1mMatrix Multiplication on GPU using OpenMP&%s\033[0m\n", compiler);
     double dtime = - omp_get_wtime();
-    mulMatrixOnACC(A, B, D, nElem);
+    #pragma omp target teams distribute \
+            parallel for collapse(2)
+    for (int i = 0; i < nElem; i++) {
+        for (int j = 0; j < nElem; j++) {
+            float temp = 0.0;
+            for (int k = 0; k < nElem; k++) {
+                temp += A[i * nElem + k] * B[k * nElem + j];
+            }
+            D[i * nElem + j] = temp;
+        }
+    }
     dtime += omp_get_wtime();
     #pragma omp target exit data map(from:D[0:nxy])
     printf("Elapsed time: %.3f sec, %.4f TFLOPS\n\n", dtime, calcMmulTFLOPS(nElem, dtime));
